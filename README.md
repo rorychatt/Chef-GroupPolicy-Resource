09ba0ecc07f4a8c83e8ed025db0614ad037ac57d
Rory Chatterton <rorychatt@gmail.com>
2017-08-27 14:34:29 +1000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chef-GroupPolicy-Resource
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: Chef-GroupPolicy-Resource
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
